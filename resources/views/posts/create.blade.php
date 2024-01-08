205af823cc4bb3b06d83a26b968a5722262add45
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-08 16:03:09 +0800

    Changing basic form using the laravelcollective form package

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,12 +3,18 @@
 @section('content')
 
 <h1>Create a Post</h1>
-    <form action="/posts" method="post">
-        {{ csrf_field() }}
-        Title: <input type="text" name="title" placeholder="Enter title"><br>
-        Content: <input type="text" name="content"><br>
-        <input type="submit" name="submit">
-    </form>
+    {!! Form::open(['method' => 'POST', 'action' => 'App\Http\Controllers\PostController@store']) !!}
+        <div class="form-group">
+            {!! Form::label('title', 'Title: ') !!}
+            {!! Form::text('title', null, ['class' => 'form-control']) !!}
+            <br>
+            {!! Form::label('content', 'Content: ') !!}
+            {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Create post', ['class' => 'btn btn-primary']) !!}
+        </div>
+    {!! Form::Close() !!}
 
 @stop
 
