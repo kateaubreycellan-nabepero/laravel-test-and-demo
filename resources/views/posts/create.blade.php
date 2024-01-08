5969de6674b6d609b4d49afe406278fa3a0d3e64
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-08 17:47:45 +0800

    Modifying create view for file input

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,15 +3,18 @@
 @section('content')
 
 <h1>Create a Post</h1>
-    {!! Form::open(['method' => 'POST', 'action' => 'App\Http\Controllers\PostController@store']) !!}
+    {!! Form::open(['method' => 'POST', 'action' => 'App\Http\Controllers\PostController@store', 'files' => true]) !!}
         <div class="form-group">
             {!! Form::label('title', 'Title: ') !!}
             {!! Form::text('title', null, ['class' => 'form-control']) !!}
             <br>
             {!! Form::label('content', 'Content: ') !!}
             {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            <br>
+            {!! Form::file('file', ['class' => 'form-control']) !!}
         </div>
         <div class="form-group">
+            <br>
             {!! Form::submit('Create post', ['class' => 'btn btn-primary']) !!}
         </div>
     {!! Form::Close() !!}
