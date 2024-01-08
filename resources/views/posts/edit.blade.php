0af676b2b004c8d0d1311f9721739a936ce8d462
Kate Aubrey Cellan <kateaubrey.cellan@nabepero.co.jp>
2024-01-08 16:18:07 +0800

    Edit the edit/delete page using the form package

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,19 +3,23 @@
 @section('content')
 
 <h1>Edit post</h1>
-    <form action="/posts/{{ $post->id }}" method="post">
-        {{ csrf_field() }}
-        <input type="hidden" name="_method" value="PUT">
-        Title: <input type="text" name="title" placeholder="Enter title" value="{{ $post->title }}"><br>
-        Content: <input type="text" name="content" value="{{ $post->content }}"><br>
-        <input type="submit" name="submit" value="Update data">
-    </form>
-
-    <form action="/posts/{{ $post->id }}" method="post">
-        <input type="hidden" name="_method" value="DELETE"><br><br><br><br>
-        <input type="submit" value="Delete. This is cannot be undone">
-    </form>
-
+    {!! Form::open(['method' => 'PATCH', 'action' => ['App\Http\Controllers\PostController@update', $post->id]]) !!}
+        <div class="form-control">
+            {!! Form::label('title', 'Title: ') !!}
+            {!! Form::text('title', $post->title, ['class' => 'form-control']) !!}
+            <br>
+            {!! Form::label('content', 'Content: ') !!}
+            {!! Form::text('content', $post->content, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-control">
+            {!! Form::submit('Update data', ['class' => 'btn btn-info']) !!}
+        </div>
+    {!! Form::close() !!}
+    <br>
+    <br>
+    {!! Form::open(['method' => 'DELETE', 'action' => ['App\Http\Controllers\PostController@destroy', $post->id]]) !!}
+        {!! Form::submit('Delete data. This cannot be undone', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
 @stop
 
 @section('footer')
